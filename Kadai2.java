ac70985989670cd6cd105b9207feecff3986e7ec
MasayoshiAizawa <67139156+MasayoshiAizawa@users.noreply.github.com>
2020-08-24 16:42:25 +0900

    Update Kadai2.java

--- Kadai2.java
+++ Kadai2.java
@@ -8,23 +8,22 @@ public class Kadai2 {
 
 		String butNumber = sc.nextLine();//嫌いな数字の入力
 		int roomNumber = Integer.parseInt(sc.nextLine());//病室の総数の入力
-		String []arrayLoom = new String[roomNumber];//病室の総数に対応する配列の作成
+		List<String> roomList new ArrayList<String>();//病室のListの作成
 
 		for(int i = 0;i < roomNumber;i++){
-			arrayLoom[i] = sc.nextLine();//病室の部屋番号の入力
+			room.List.add(sc.nextLine());//上記のListに部屋番号を入力
 		}
-
-		List<String> roomList = Arrays.asList(arrayLoom);//上記の配列をList化
-
+		
+		sc.close();
+		
 		List<String> roomListsub = roomList.stream()
 		.filter(num -> num.indexOf(butNumber) < 0)//部屋番号から嫌いな数字を取り除く
 		.collect(Collectors.toList());//filterをしたものを再List化
 
 		roomListsub.stream().forEach(System.out::println);//条件に一致する部屋番号の表示
 
-		if(roomListsub.stream().allMatch(s -> s == null)){//すべての部屋番号が条件に一致しない場合の出力
+		if(roomListsub.isEmpty()){//すべての部屋番号が条件に一致しない場合の出力
 				System.out.println("none");
 		}
-		sc.close();
 	}
 }
