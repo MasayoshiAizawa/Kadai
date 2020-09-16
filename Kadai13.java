047bd127f1569fc2908c1fbaa13641da195c8e46
MasayoshiAizawa <67139156+MasayoshiAizawa@users.noreply.github.com>
2020-09-16 18:50:57 +0900

    Add files via upload

---
+++ Kadai13.java
@@ -0,0 +1,48 @@
+package practice009;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
+
+public class Kadai13 {
+	public static void main (String args[]){
+		
+		List<String> stringlist = new ArrayList<>();
+		
+		for(int i = 0; i < 5; i++){
+			stringlist.add("処理対象" + (i +1));
+		}
+		
+		//同時実行する処理をリスト化
+		var list = List.of(
+			for(String proc : stringlist){
+				ParallelProcessSub1 par1 = new ParallelProcessSub1(proc);
+				ParallelProcessSub2 par2 = new ParallelProcessSub2(proc);
+				ParallelProcessSub3 par3 = new ParallelProcessSub3(proc);
+				CompletableFuture.supplyAsync(() -> par1.process());
+				CompletableFuture.supplyAsync(() -> par2.process());
+				CompletableFuture.supplyAsync(() -> par3.process());
+				CompletableFuture.supplyAsync(() -> System.out.println(proc + "実行済み"));
+			}
+		);
+		
+		//複数の処理を実行
+		CompletableFuture.allOf(list.toArray(new CompletableFuture[list.size()]))
+			//すべて完了後
+			.whenComplete((result,ex) -> {
+				if(ex == null){
+					//順に結果を出力
+					list.forEach((future) -> {
+						System.out.println(future.get());
+					});
+				}else{
+					System.out.println(ex.getMessage());
+				}
+			});
+	}
+}
