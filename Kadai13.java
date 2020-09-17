21ec39a6bba89f090a05f6d721c1793b4947273e
MasayoshiAizawa <67139156+MasayoshiAizawa@users.noreply.github.com>
2020-09-17 17:29:36 +0900

    Update Kadai13.java

--- Kadai13.java
+++ Kadai13.java
@@ -2,44 +2,35 @@ package practice009;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Kadai13 {
 	public static void main (String args[]){
-		
-		List<String> stringlist = new ArrayList<>();
-		
+
+		List<CompletableFuture<String>> list = new ArrayList<>();
 		for(int i = 0; i < 5; i++){
-			stringlist.add("処理対象" + (i +1));
+			String proc = ("処理対象" + (i +1));
+			System.out.println(proc + "実行済み");
+				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub1(proc).process()));
+				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub2(proc).process()));
+				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub3(proc).process()));
 		}
-		
-		//同時実行する処理をリスト化
-		for(String proc : stringlist){
-			var list = List.of(
-				CompletableFuture.supplyAsync(() -> new ParallelProcessSub1(proc).process()),
-				CompletableFuture.supplyAsync(() -> new ParallelProcessSub2(proc).process()),
-				CompletableFuture.supplyAsync(() -> new ParallelProcessSub3(proc).process()),
-				CompletableFuture.supplyAsync(() -> System.out.println(proc + "実行済み"))
-			);
-		}
-		
-		//複数の処理を実行
-		CompletableFuture.allOf(list.toArray(new CompletableFuture[list.size()]))
-			//すべて完了後
-			.whenComplete((result,ex) -> {
-				if(ex == null){
-					//順に結果を出力
+
+	CompletableFuture.allOf(
+		list.toArray(new CompletableFuture[list.size()]))
+		.whenComplete((result,ex) -> {
+			if(ex == null){
 					list.forEach((future) -> {
-						System.out.println(future.get());
+						try {
+							System.out.println(future.get());
+						} catch (InterruptedException | ExecutionException e) {
+							e.printStackTrace();
+						}
 					});
-				}else{
-					System.out.println(ex.getMessage());
-				}
-			});
+			}else{
+				System.out.println("err=" + ex);
+			}
+		});
 	}
 }
