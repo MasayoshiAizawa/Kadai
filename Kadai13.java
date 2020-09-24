65cd78560bccddc56301831b6b3945d0f08fc4eb
MasayoshiAizawa <67139156+MasayoshiAizawa@users.noreply.github.com>
2020-09-24 20:22:12 +0900

    Update Kadai13.java

--- Kadai13.java
+++ Kadai13.java
@@ -3,7 +3,7 @@ package practice009;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class Kadai13 {
 	public static void main (String args[]){
@@ -11,32 +11,37 @@ public class Kadai13 {
 		long startTime = System.currentTimeMillis();
 
 		//同時実行する処理のリスト化
-		List<CompletableFuture<String>> list = new ArrayList<>();
+		List<Future<String>> list = new ArrayList<>();
 		for(int i = 0; i < 5; i++){
 			String proc = ("処理対象" + (i +1));
 			System.out.println(proc + "実行済み");
-				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub1(proc).process()));
-				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub2(proc).process()));
-				list.add(CompletableFuture.supplyAsync(() -> new ParallelProcessSub3(proc).process()));
+				CompletableFuture.supplyAsync(() -> {
+					ParallelProcessSub1 sub1 = new ParallelProcessSub1(proc);
+					return sub1.process();
+				})
+					.thenAcceptAsync((result) -> {
+						list.add(result);
+					});
+				CompletableFuture.supplyAsync(() -> {
+					ParallelProcessSub2 sub2 = new ParallelProcessSub2(proc);
+					return sub2.process();
+				})
+					.thenAcceptAsync((result) -> {
+						list.add(result);
+					});
+				CompletableFuture.supplyAsync(() -> {
+					ParallelProcessSub3 sub3 = new ParallelProcessSub3(proc);
+					return sub3.process();
+				})
+					.thenAcceptAsync((result) -> {
+						list.add(result);
+					});
 		}
 
-		//複数の処理の実行
-		CompletableFuture.allOf(
-				list.toArray(new CompletableFuture[list.size()]))
-			.whenComplete((result,ex) -> {
-				if(ex == null){
-					//順に結果を表示
-					list.forEach((future) -> {
-						try {
-							System.out.println(future.get());
-						} catch (InterruptedException | ExecutionException e) {
-							e.printStackTrace();
-						}
-					});
-			}else{
-				System.out.println("err=" + ex);
-			}
-		});
+		for(Future<String> se : list){
+				System.out.println(se.get());
+		};
+
 		long endTime = System.currentTimeMillis();
 		System.out.println("実行時間" + (endTime -startTime));
 	}
